b779b7d75c9dae81df6f2a3943f51d6efe532ca6
Davi Ferreira <contato@daviferreira.com>
2011-07-29 11:26:41 -0300

    indenting remove directory

--- remove_directory.php
+++ remove_directory.php
@@ -2,25 +2,25 @@
 function remove_directory($directory, $empty = false){
   $directory = preg_replace('/\/$/', '', $directory);
 
-	if(!file_exists($directory) || !is_dir($directory) || !is_readable($directory)){
-		return false;
+  if(!file_exists($directory) || !is_dir($directory) || !is_readable($directory)){
+    return false;
   }else{
-		$handle = opendir($directory);
-		while(false !== ($item = readdir($handle))){
-			if($item != '.' && $item != '..'){
-				$path = $directory.'/'.$item;
-				if(is_dir($path)) 
-					remove_directory($path);
-				else
-					unlink($path);
-			}
-		}
-		closedir($handle);
-    
+    $handle = opendir($directory);
+    while(false !== ($item = readdir($handle))){
+      if($item != '.' && $item != '..'){
+        $path = $directory.'/'.$item;
+        if(is_dir($path)) 
+          remove_directory($path);
+        else
+          unlink($path);
+      }
+    }
+    closedir($handle);
+
     if(!$empty)
-			if(!rmdir($directory))
-				return false;
-    
+      if(!rmdir($directory))
+        return false;
+
     return true;
-	}
+  }
 }
