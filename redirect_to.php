1a8536096812a61bece6b717852214e126570354
Davi Ferreira <contato@daviferreira.com>
2011-07-29 10:35:04 -0300

    Redirect to

---
+++ redirect_to.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Redirects to specified address 
+ * If headers were already sent, uses javascript 
+ *
+ * @param String $location URL or path to redirect
+ * @return void
+ * @author Davi Ferreira
+ **/
+function redirect_to($location){
+	if(!headers_sent())
+		header("Location: {$location}");
+	else
+		echo "<script>window.location.href='{$location}';</script>";
+	exit;
+}
