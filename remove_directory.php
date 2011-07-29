4fcca6aed1903f0a895a8fec2331b6133ca9a239
Davi Ferreira <contato@daviferreira.com>
2011-07-29 11:25:04 -0300

    Added remove_directory and snippet_chars

---
+++ remove_directory.php
@@ -0,0 +1,26 @@
+<?php
+function remove_directory($directory, $empty = false){
+  $directory = preg_replace('/\/$/', '', $directory);
+
+	if(!file_exists($directory) || !is_dir($directory) || !is_readable($directory)){
+		return false;
+  }else{
+		$handle = opendir($directory);
+		while(false !== ($item = readdir($handle))){
+			if($item != '.' && $item != '..'){
+				$path = $directory.'/'.$item;
+				if(is_dir($path)) 
+					remove_directory($path);
+				else
+					unlink($path);
+			}
+		}
+		closedir($handle);
+    
+    if(!$empty)
+			if(!rmdir($directory))
+				return false;
+    
+    return true;
+	}
+}
---
+++ snippet_chars.php
@@ -0,0 +1,8 @@
+<?php
+function snippet_chars($phrase, $start, $max_chars, $delimiter = "&hellip;"){
+  if(strlen($phrase) > $max_chars) {
+    $phrase = substr($phrase, $start, $max_chars);
+    $phrase .= ' '.$delimiter;
+  }
+  return $phrase;
+}
