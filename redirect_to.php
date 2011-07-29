077eae278c0b53a19a4d56442099ae6c0f68dead
Davi Ferreira <contato@daviferreira.com>
2011-07-29 11:12:27 -0300

    Comments are for the weak

--- redirect_to.php
+++ redirect_to.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Redirects to specified address 
- * If headers were already sent, uses javascript 
- *
- * @param String $location URL or path to redirect
- * @return void
- **/
 function redirect_to($location){
   if(!headers_sent())
     header("Location: {$location}");
--- slugify.php
+++ slugify.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * Convert string to slug (ie. friendly URLs)
- * Requires strip_accents
- *
- * @param String $str to be converted 
- * @return String generated slug 
- **/
+// Requires strip_accents
 function slugify($str){
   $str = preg_replace('/[^a-z0-9-]/', '-', strtolower(trim(strip_accents($str))));
   return preg_replace('/-+/', "-", $str);
