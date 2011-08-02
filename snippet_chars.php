2979097216584b4e380ebca4a398127c3874eb78
Davi Ferreira <contato@daviferreira.com>
2011-08-02 13:09:42 -0300

    Snippet chars, space is now part of delimiter

--- snippet_chars.php
+++ snippet_chars.php
@@ -1,6 +1,6 @@
 <?php
-function snippet_chars($phrase, $start, $max_chars, $delimiter = "&hellip;"){
+function snippet_chars($phrase, $start, $max_chars, $delimiter = " &hellip;"){
   if(strlen($phrase) > $max_chars)
-    $phrase = substr($phrase, $start, $max_chars).' '.$delimiter;
+    $phrase = substr($phrase, $start, $max_chars).$delimiter;
   return $phrase;
 }
