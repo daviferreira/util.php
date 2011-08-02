36aab4ad9d82e6dfcb8058edafc3ca2528031f3e
Davi Ferreira <contato@daviferreira.com>
2011-08-02 13:08:57 -0300

    Snippet chars refactored

--- snippet_chars.php
+++ snippet_chars.php
@@ -1,8 +1,6 @@
 <?php
 function snippet_chars($phrase, $start, $max_chars, $delimiter = "&hellip;"){
-  if(strlen($phrase) > $max_chars) {
-    $phrase = substr($phrase, $start, $max_chars);
-    $phrase .= ' '.$delimiter;
-  }
+  if(strlen($phrase) > $max_chars)
+    $phrase = substr($phrase, $start, $max_chars).' '.$delimiter;
   return $phrase;
 }
